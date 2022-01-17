a99c794c3a1c3d22a11207829ff4f12d4c9a196c
vanshika0210 <thakralvanshika210@gmail.com>
2022-01-17 10:43:43 +0530

    last commit

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -1,20 +1,27 @@
 <?php 
 
 namespace App\Controller;
-use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class QuestionController extends AbstractController
+
+class CommentController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}/vote/{direction}")
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id,$direction)
+
+    public function commentVote($id,$direction,LoggerInterface $logger)
     {
+        
         if ($direction === 'up') {
+            $logger->info('Voting Up!');
             $currentVoteCount = rand(7, 100);
         } else {
+            $logger->info('Voting Down!');
             $currentVoteCount = rand(0, 5);
         }
         return new JsonResponse(['votes' => $currentVoteCount]);
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,9 +1,12 @@
 <?php 
 namespace App\Controller;
-
+use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+
 
 class QuestionController extends AbstractController
 {
@@ -11,27 +14,50 @@ class QuestionController extends AbstractController
     * @Route("/",name="app_homepage")
     */
 
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
-        return $this->render('question/homepage.html.twig');
+        $html=$twigEnvironment->render('question/homepage.html.twig');
+        return new Response($html);
+       // return $this->render('question/homepage.html.twig');
     }
     /**
     * @Route("/questions/{slug}",name="app_question_show")
     */
 
-    public function show($slug)
+    public function show($slug,MarkdownParserInterface $markdownParser,CacheInterface $cache)
     {
-        $answers=[            
-            'Make sure your cat is sitting purrrfectly still 🤣',
-        'Honestly, I like furry shoes better than MY cat',
-        'Maybe... try saying the spell backwards?',
 
+        $answers=
+        [            
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
         ];
-        //return new Response('Hello vanshika,How are you!');
-        return $this->render('question/show.html.twig',['question'=>ucwords(str_replace('-',' ',$slug)),
-        'answers'=>$answers,
+
+        $questionText = 
+        'I\'ve been turned into a cat, any thoughts 
+        on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+
+        //$parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+
+        $parsedQuestionText = 
+        $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
+            return $markdownParser->transformMarkdown($questionText);
+
+        });
+        //dd($markdownParser);
+        //return new Response('Hello vanshika, How are you!');
+         
+        //return $this->render('question/show.html.twig',['question'=>ucwords(str_replace('-',' ',$slug)),'answers'=>$answers,
+    //]);
+    return $this->render('question/show.html.twig', [
+        'question' => ucwords(str_replace('-', ' ', $slug)),
+        'questionText' => $parsedQuestionText,
+        'answers' => $answers,
     ]);
     }
+
+    
 
     
 
